302dd8acfdde277926a0835a8f1341f08ede2e6b
judithandraskoMaisonneuve <148014415+judithandraskoMaisonneuve@users.noreply.github.com>
2024-10-31 13:34:58 -0400

    Update projetIOT.ino

--- projetIOT/projetIOT.ino
+++ projetIOT/projetIOT.ino
@@ -1,8 +1,7 @@
-#include <DHT.h>
 #include <Servo.h>
 
-Servo monServo;    // First servo motor
-Servo secondServo; // Second servo motor
+Servo monServo;       // First servo motor
+Servo secondServo;    // Second servo motor
 
 #define echoPin 2
 #define trigPin 3
@@ -11,55 +10,84 @@ const int buzzer = 6;  // Buzzer pin
 long duration;         // Time for the echo to return
 int distance;          // Calculated distance
 
+bool isObjectDetected = false;  // State variable for object detection
+
 void setup() {
   Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);  
-  pinMode(echoPin, INPUT);   
-  pinMode(buzzer, OUTPUT);    
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzer, OUTPUT);
 
-  monServo.attach(5);         
-  secondServo.attach(4);      
-  monServo.write(0);          
-  secondServo.write(0);       
-  delay(500);                 
+  monServo.attach(5);
+  secondServo.attach(4);
+  monServo.write(0);
+  secondServo.write(0);
+  delay(500);
 }
 
 void loop() {
-  digitalWrite(trigPin, LOW);  
-  delayMicroseconds(2);        
-  digitalWrite(trigPin, HIGH); 
-  delayMicroseconds(10);       
-  digitalWrite(trigPin, LOW);  
+  distance = getDistance();
 
-  duration = pulseIn(echoPin, HIGH);  
-  distance = duration * 0.034 / 2;    
-
+  // Print distance reading
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
 
-  if (distance <= 5) {
+  if (distance <= 5 && !isObjectDetected) {
+    isObjectDetected = true;
     activateBuzzer(true);
-    moveServos(0, 190);
-    moveServos(190, 0);
-  } else {
+
+    // Print object detection status and buzzer state
+    Serial.println("Object detected within 5 cm!");
+    Serial.println("Buzzer: ON");
+
+    oscillateServos();
+  } else if (distance > 5 && isObjectDetected) {
+    isObjectDetected = false;
     activateBuzzer(false);
-    moveServos(0, 0);
+
+    // Print buzzer state when it turns off
+    Serial.println("No object detected within 5 cm.");
+    Serial.println("Buzzer: OFF");
+
+    resetServos();
   }
 
-  delay(500);  
+  delay(500);
+}
+
+int getDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  return duration * 0.034 / 2;
 }
 
 void activateBuzzer(bool state) {
   digitalWrite(buzzer, state ? HIGH : LOW);
 }
 
-void moveServos(int startPos, int endPos) {
-  for (int pos = startPos; pos <= endPos; pos++) {  
+void oscillateServos() {
+  for (int pos = 0; pos <= 180; pos++) {
     monServo.write(pos);
-    secondServo.write(pos); 
-    delay(1);  
+    secondServo.write(pos);
+    delay(10);  // Adjust the speed of movement here
+  }
+  for (int pos = 180; pos >= 0; pos--) {
+    monServo.write(pos);
+    secondServo.write(pos);
+    delay(10);
   }
 }
+
+void resetServos() {
+  monServo.write(0);
+  secondServo.write(0);
+}
+
 
 
